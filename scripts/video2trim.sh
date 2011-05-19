14d19c4e0f7537b46ace6f0b31a535e6cbc709ee
mauromereu@gmail.com <mauromereu@gmail.com@b9a0ea63-8f4e-de66-d611-630790e70740>
2011-05-19 09:57:15 +0000

    video2trim now takes filenames with blanks

--- scripts/video2trim.sh
+++ scripts/video2trim.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-show=`basename $1`
+show=`basename "$1"`
+_show=$( echo "$show" | sed -e 's/ /_/g' | sed -e 's/\\//g' ) 
+mv "$show" "$_show"
+show=$_show
+#echo $show
+#exit 1
 
-if [ -f $show ] ; then
+if [ -f "$show" ] ; then
     # name without extension
     name=${show%\.*}
-    echo ${name} >> output.txt
-  fi ;
+    echo ${name} 
+fi ;
 
-gst-launch-0.10 filesrc location=$show ! decodebin  ! audioresample ! 'audio/x-raw-int,rate=16000' !  audioconvert ! 'audio/x-raw-int,rate=16000,depth=16,signed=true,channels=1'    ! wavenc !   filesink location=${name}.wav
+gst-launch-0.10 filesrc location="$show" ! decodebin  ! audioresample ! 'audio/x-raw-int,rate=16000' !  audioconvert ! 'audio/x-raw-int,rate=16000,depth=16,signed=true,channels=1'    ! wavenc !   filesink location=${name}.wav
 
 ./wav2label.sh ${name}.wav 
 ./seg2trim.sh ${name}.seg
