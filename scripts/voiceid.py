1b995dd37c92b7c1696181f23ed56b84bd0e187a
mauromereu@gmail.com <mauromereu@gmail.com@b9a0ea63-8f4e-de66-d611-630790e70740>
2011-07-06 13:18:07 +0000

    extract_speakers.py renamed as voiceid.py

--- scripts/voiceid.py
+++ scripts/voiceid.py
@@ -7,17 +7,26 @@ import sys, signal
 import time
 import re
 p = None
-clusters = {}
+verbose = False
 
-"""
-def termHandler(a,b):
-	print "termHandler"
-	p.kill()
-
-signal.signal(signal.SIGINT, termHandler )
-"""
+lium_jar='./LIUM_SpkDiarization.jar'
+db_dir = './gmm_db'
 
 dev_null = open('/dev/null','w')
+if verbose:
+	dev_null = None
+
+def start_subprocess(commandline):
+	args = shlex.split(commandline)
+	p = subprocess.Popen(args, stdout=dev_null, stderr=dev_null)
+	retval = p.wait()
+	if retval != 0: 
+		raise Exception("Subprocess closed unexpectedly "+str(p) )
+def ensure_file_exists(filename):
+	if not os.path.exists(filename):
+		raise Exception("File %s not correctly created"  % filename)
+	if not (os.path.getsize(filename) > 0):
+		raise Exception("File %s empty"  % filename)
 
 def humanize_time(secs):
 	mins, secs = divmod(secs, 60)
@@ -37,13 +46,11 @@ def video2wav(show):
 
 	name, ext = os.path.splitext(show)
 	if ext != '.wav' or is_bad_wave(show):
-		pipeline = "gst-launch-0.10 filesrc location='"+show+"' ! decodebin ! audioresample ! 'audio/x-raw-int,rate=16000' ! audioconvert ! 'audio/x-raw-int,rate=16000,depth=16,signed=true,channels=1' ! wavenc ! filesink location="+name+".wav " 
-		args = shlex.split(pipeline)
-		p = subprocess.Popen(args, stdout=dev_null)
-		retval = p.wait()
-		if retval != 0: 
-			raise Exception("Subprocess closed unexpectedly "+str(p) )
+		start_subprocess( "gst-launch-0.10 filesrc location='"+show+"' ! decodebin ! audioresample ! 'audio/x-raw-int,rate=16000' ! audioconvert ! 'audio/x-raw-int,rate=16000,depth=16,signed=true,channels=1' ! wavenc ! filesink location="+name+".wav " )
 
+
+def diarization(showname):
+	start_subprocess( 'java -Xmx2024m -jar '+lium_jar+'   --fInputMask=%s.wav --sOutputMask=%s.seg --doCEClustering '+  showname )
 
 def seg2trim(segfile):
 	basename, extension = os.path.splitext(segfile)
@@ -62,11 +69,7 @@ def seg2trim(segfile):
 				else:
 					raise os.error
 			commandline = "sox %s.wav %s/%s/%s_%s.wav trim  %s %s" % ( basename, basename, clust, clust, st, st, end )
-			args = shlex.split(commandline)
-			p = subprocess.Popen(args)
-			retval = p.wait()
-			if retval != 0:
-				raise Exception("Subprocess closed unexpectedly "+str(p) )
+			start_subprocess(commandline)
 	s.close()
 
 def seg2srt(segfile):
@@ -86,7 +89,7 @@ def seg2srt(segfile):
 	s = open(segfile,'r')
 	lines=readl(s)
 	lines.sort(key=readtime, reverse=False)
-        fileoutput = basename+".srt"
+	fileoutput = basename+".srt"
 	FILE = open(fileoutput,"w")
 	row = 0
 	for line in lines:
@@ -98,34 +101,61 @@ def seg2srt(segfile):
 			
 	FILE.close()
 
-
-def replace_words(text, word_dic):
-    """
-    take a text and replace words that match a key in a dictionary with
-    the associated value, return the changed text
-    """
-    rc = re.compile('|'.join(map(re.escape, word_dic)))
-	
-    def translate(match):
-        return word_dic[match.group(0)]+'\n'
-    
-    return rc.sub(translate, text)
-
 def video2trim(videofile):
+	print "*** converting video to wav ***"
 	video2wav(videofile)
 	show, ext = os.path.splitext(videofile)
+	print "*** diarization ***"
+	diarization(show)
+	print "*** trim ***"
 	seg2trim(show+'.seg')
 
-def extract_mfcc(inputfile):
-	show, ext = os.path.splitext(inputfile)
+def extract_mfcc(show):
 	commandline = "sphinx_fe -verbose no -mswav yes -i %s.wav -o %s.mfcc" %  ( show, show )
-	args = shlex.split(commandline)
-	p = subprocess.Popen(args, stdout=dev_null, stderr=dev_null) 
-        retval = p.wait()
-	if retval != 0:
-		raise Exception("Subprocess closed unexpectedlyi "+str(p) )
+	start_subprocess(commandline)
 
-def srt2subnames(original_subtitle, showname, key_value):
+def ident_seg(showname,name):
+	f = open(showname+'.seg','r')
+	clusters=[]
+	lines = f.readlines()
+	for line in lines:
+		for k in line.split():
+			if k.startswith('cluster:'):
+				prefix,c = k.split(':')
+				clusters.append(c)
+	f.close()
+	output = open(showname+'ident.seg', 'w')
+	clusters.reverse()
+	for line in lines:
+		for c in clusters:
+			line.replace(c,name)
+		output.write(line+'\n')
+	output.close()
+
+def train_init(show):
+	commandline = 'java -Xmx2024m -cp '+lium_jar+' fr.lium.spkDiarization.programs.MTrainInit --help --sInputMask=%s.ident.seg --fInputMask=%s.wav --fInputDesc="audio16kHz2sphinx,1:3:2:0:0:0,13,1:1:300:4"  --emInitMethod=copy --tInputMask=./ubm.gmm --tOutputMask=%s.init.gmm '+show
+	start_subprocess(commandline)
+
+def train_map(show):
+	commandline = 'java -Xmx2024m -cp '+lium_jar+' fr.lium.spkDiarization.programs.MTrainMAP --help --sInputMask=%s.ident.seg --fInputMask=%s.mfcc --fInputDesc="audio16kHz2sphinx,1:3:2:0:0:0,13,1:1:300:4"  --tInputMask=%s.init.gmm --emCtrl=1,5,0.01 --varCtrl=0.01,10.0 --tOutputMask=%s.gmm ' + show 
+	start_subprocess(commandline)
+
+def srt2subnames(showname, key_value):
+	def replace_words(text, word_dic):
+	    """
+	    take a text and replace words that match a key in a dictionary with
+	    the associated value, return the changed text
+	    """
+	    rc = re.compile('|'.join(map(re.escape, word_dic)))
+		
+	    def translate(match):
+		return word_dic[match.group(0)]+'\n'
+	    
+	    return rc.sub(translate, text)
+
+	file_original_subtitle = open(showname+".srt") #FIXME: open file in function
+	original_subtitle = file_original_subtitle.read()
+	file_original_subtitle.close()
 	key_value=dict(map(lambda (key, value): (str(key)+"\n", value), key_value.items()))
 	str3 = replace_words(original_subtitle, key_value)
 	out_file = showname+"_new.srt"
@@ -134,7 +164,7 @@ def srt2subnames(original_subtitle, showname, key_value):
 	fout.write(str3)
 	fout.close()	
 
-def extract_clusters(filename):
+def extract_clusters(filename, clusters):
 	f = open(filename,"r")
 	for l in f:
 		 if l.startswith(";;") :
@@ -143,14 +173,11 @@ def extract_clusters(filename):
 	f.close()
 
 def mfcc_vs_gmm(showname, gmm):
-	commandline = 'java -Xmx2G -Xms2G -cp ./LIUM_SpkDiarization.jar  fr.lium.spkDiarization.programs.MScore --sInputMask=%s.seg   --fInputMask=%s.mfcc  --sOutputMask=%s.ident.'+gmm+'.seg --sOutputFormat=seg,UTF8  --fInputDesc="audio16kHz2sphinx,1:3:2:0:0:0,13,1:0:300:4" --tInputMask=./gmm_db/'+gmm+' --sTop=8,ubm.gmm  --sSetLabel=add --sByCluster '+  showname 
-	args = shlex.split(commandline)
-	p = subprocess.Popen(args)
-	retval = p.wait()
-	if retval != 0:
-		raise Exception("Subprocess closed unexpectedlyi "+str(p) )
+	commandline = 'java -Xmx2G -Xms2G -cp '+lium_jar+'  fr.lium.spkDiarization.programs.MScore --sInputMask=%s.seg   --fInputMask=%s.mfcc  --sOutputMask=%s.ident.'+gmm+'.seg --sOutputFormat=seg,UTF8  --fInputDesc="audio16kHz2sphinx,1:3:2:0:0:0,13,1:0:300:4" --tInputMask='+db_dir+'/'+gmm+' --sTop=8,ubm.gmm  --sSetLabel=add --sByCluster '+  showname 
+	start_subprocess(commandline)
 
-def manage_ident(showname, gmm):
+
+def manage_ident(showname, gmm, clusters):
 	f = open("%s.ident.%s.seg" % (showname,gmm ) ,"r")
 	for l in f:
 		 if l.startswith(";;"):
@@ -168,17 +195,30 @@ def manage_ident(showname, gmm):
 def wave_duration(wavfile):
 	import wave
 	w = wave.open(wavfile)
-	p = w.getparams()
+	par = w.getparams()
 	w.close()
-	return p[3]/p[2]
+	return par[3]/par[2]
+
+def build_gmm(show,name):
+	
+	diarization(show)
+
+	ident_seg(show,name)
+
+	extract_mfcc(show)
+
+	train_init(show)
+
+	train_map(show)
+
 
 
 if __name__ == '__main__':
 	cpus = cpu_count()
-	print '%s processors' % cpus
+	clusters = {}
+	#print '%s processors' % cpus
 	start_time = time.time()
 	file_input = sys.argv[ 1: ] 
-	print file_input
 	if len( file_input ) > 1: #blanks in file name
 		print file_input
 		new_file_input = '_'.join(file_input).replace("'",'_').replace('-','_')
@@ -191,13 +231,16 @@ if __name__ == '__main__':
 
 	video2trim( file_input )
 	basename, extension = os.path.splitext( file_input )
-	extract_mfcc( file_input )
+	seg2srt(basename+'.seg')
+	extract_mfcc( basename )
 	
-	extract_clusters( "%s.seg" %  basename )
+	print "*** voice matching ***"
+	extract_clusters( "%s.seg" %  basename, clusters )
 	p = {}
-	for f in os.listdir('./gmm_db'):
+	files_in_db = [ f for f in os.listdir(db_dir) if f.endswith('.gmm') ]
+	for f in files_in_db:
 		#print "len(active_children()) %d" % len(active_children() )
-		if f.endswith('.gmm') and len(active_children()) < (cpus-1) :
+		if  len(active_children()) < (cpus-1) :
 			p[f] = Process(target=mfcc_vs_gmm, args=( basename, f ) )
 			p[f].start()
 		else:
@@ -206,19 +249,17 @@ if __name__ == '__main__':
 				time.sleep(1)	
 			p[f] = Process(target=mfcc_vs_gmm, args=( basename, f ) )
 			p[f].start()
-			#mfcc_vs_gmm( basename, f )
 	for proc in p:
 		if p[proc].is_alive():
 			print 'joining proc %s' % str(proc)
 			p[proc].join()	
 	
 	#print "Processes %s" % active_children()
-	for f in os.listdir('./gmm_db'):
-		if f.endswith('.gmm'):
-			manage_ident( basename, f )
+	for f in files_in_db:
+		manage_ident( basename, f , clusters)
 	print ""
 	speakers = {}
-	print clusters
+	#print clusters
 	for c in clusters:
 	    print c
 	    value = -33.0
@@ -241,9 +282,8 @@ if __name__ == '__main__':
 		    best = 'unknown'
 		    speakers[c] = best
 	    print '\t best speaker: %s (distance from 2nd %f - mean %f - distance from mean %f ) ' % (best , distance, mean, m_distance)
-	file_original_subtitle = open(basename+".srt")
-        srt2subnames(file_original_subtitle.read(), basename, speakers)
+        srt2subnames(basename, speakers)
 	sec = wave_duration(basename+'.wav')
 	total_time = time.time() - start_time
-	print "\nwav duration: %s\nall done in %dsec (%s) with %s cpus" % ( time.strftime('%H:%M:%S',time.gmtime(sec)), total_time,  time.strftime('%H:%M:%S', time.gmtime(total_time)), cpus )
+	print "\nwav duration: %s\nall done in %dsec (%s) with %s cpus" % ( humanize_time(sec), total_time, humanize_time(total_time), cpus )
 
