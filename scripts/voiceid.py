596ad8f95793311702c04981dcd5ade0030435c6
mauromereu@gmail.com <mauromereu@gmail.com@b9a0ea63-8f4e-de66-d611-630790e70740>
2011-07-07 15:43:33 +0000

    some enhancements

--- scripts/voiceid.py
+++ scripts/voiceid.py
@@ -16,10 +16,12 @@ db_dir = './gmm_db'
 keep_intermediate_files = False
 
 dev_null = open('/dev/null','w')
+
 if verbose:
 	dev_null = None
 
 def start_subprocess(commandline):
+	""" Starts a subprocess using the given commandline and check for correct termination """
 	args = shlex.split(commandline)
 	p = subprocess.Popen(args, stdout=dev_null, stderr=dev_null)
 	retval = p.wait()
@@ -27,17 +29,27 @@ def start_subprocess(commandline):
 		raise Exception("Subprocess closed unexpectedly "+str(p) )
 
 def ensure_file_exists(filename):
+	""" Ensure file exists and not empty """
 	if not os.path.exists(filename):
 		raise Exception("File %s not correctly created"  % filename)
 	if not (os.path.getsize(filename) > 0):
 		raise Exception("File %s empty"  % filename)
 
+def  check_deps():
+	""" Check for dependency """
+	import magic
+	ensure_file_exists(lium_jar)
+	if not os.path.exists(db_dir):
+		raise Exception("No gmm db directory found in %s (take a look to the configuration, db_dir parameter)" % db_dir )
+
 def humanize_time(secs):
+	""" Convert seconds into time format """
 	mins, secs = divmod(secs, 60)
 	hours, mins = divmod(mins, 60)
 	return '%02d:%02d:%02d,%s' % (hours, mins, int(secs), str(("%0.3f" % secs ))[-3:] )
 
 def video2wav(show):
+	""" Takes any kind of video or audio and convert it to a "RIFF (little-endian) data, WAVE audio, Microsoft PCM, 16 bit, mono 16000 Hz" wave file using gstreamer. If you call it passing a wave it checks if in good format, otherwise it converts the wave in the good format """
 	def is_bad_wave(show):
 		import magic
 		ms = magic.open(magic.MAGIC_NONE)
@@ -55,10 +67,12 @@ def video2wav(show):
 
 
 def diarization(showname):
-	start_subprocess( 'java -Xmx2024m -jar '+lium_jar+'   --fInputMask=%s.wav --sOutputMask=%s.seg --doCEClustering '+  showname )
+	""" Takes a wave file in the correct format and build a segmentation file. The seg file shows how much speakers are in the audio and when they talk """
+	start_subprocess( 'java -Xmx2024m -jar '+lium_jar+'   --fInputMask=%s.wav --sOutputMask=%s.seg --doCEClustering ' +  showname )
 	ensure_file_exists(showname+'.seg')
 
 def seg2trim(segfile):
+	""" Take a wave and splits it in small waves in this directory structure <file base name>/<cluster>/<cluster>_<start time>.wav """
 	basename, extension = os.path.splitext(segfile)
 	s = open(segfile,'r')
 	for line in s.readlines():
@@ -80,36 +94,35 @@ def seg2trim(segfile):
 	s.close()
 
 def seg2srt(segfile):
-	def readl(s):
-		total = []
-		for line in s.readlines():
-			if not line.startswith(";;"):
-				arr=line.split()
-				total.append(arr)
-		s.close()
-		return total
-
+	""" Takes a seg file and convert it in a subtitle file (srt) """
 	def readtime(aline):
 		return int(aline[2])
 
 	basename, extension = os.path.splitext(segfile)	
 	s = open(segfile,'r')
-	lines=readl(s)
+	lines = []
+	for line in s.readlines():
+		if not line.startswith(";;"):
+			arr=line.split()
+			lines.append(arr)
+	s.close()
+	
 	lines.sort(key=readtime, reverse=False)
 	fileoutput = basename+".srt"
-	FILE = open(fileoutput,"w")
+	srtfile = open(fileoutput,"w")
 	row = 0
 	for line in lines:
 		row = row +1
-		FILE.write(str(row)+"\n")
-		FILE.write(humanize_time(float(line[2])) + " --> " + humanize_time(float(line[2])+float(line[3]))+"\n")
-		FILE.write(line[7]+"\n")
-		FILE.write(""+"\n")
+		srtfile.write(str(row)+"\n")
+		srtfile.write(humanize_time(float(line[2])) + " --> " + humanize_time(float(line[2])+float(line[3]))+"\n")
+		srtfile.write(line[7]+"\n")
+		srtfile.write(""+"\n")
 			
-	FILE.close()
+	srtfile.close()
 	ensure_file_exists(basename+'.srt')
 
 def video2trim(videofile):
+	""" Takes a video or audio file and converts it into smaller waves according to the diarization process """
 	print "*** converting video to wav ***"
 	video2wav(videofile)
 	show, ext = os.path.splitext(videofile)
@@ -119,11 +132,13 @@ def video2trim(videofile):
 	seg2trim(show+'.seg')
 
 def extract_mfcc(show):
+	""" Extract audio features from the wave file, in particular the mel-frequency cepstrum using a sphinx tool """
 	commandline = "sphinx_fe -verbose no -mswav yes -i %s.wav -o %s.mfcc" %  ( show, show )
 	start_subprocess(commandline)
 	ensure_file_exists(show+'.mfcc')
 
 def ident_seg(showname,name):
+	""" Takes a seg file and substitute the clusters with a given name or identifier """
 	f = open(showname+'.seg','r')
 	clusters=[]
 	lines = f.readlines()
@@ -153,6 +168,7 @@ def train_map(show):
 	ensure_file_exists(show+'.gmm')
 
 def srt2subnames(showname, key_value):
+	""" Substitute cluster names with real names in subtitles """
 	def replace_words(text, word_dic):
 	    """
 	    take a text and replace words that match a key in a dictionary with
@@ -169,15 +185,16 @@ def srt2subnames(showname, key_value):
 	original_subtitle = file_original_subtitle.read()
 	file_original_subtitle.close()
 	key_value=dict(map(lambda (key, value): (str(key)+"\n", value), key_value.items()))
-	str3 = replace_words(original_subtitle, key_value)
+	text = replace_words(original_subtitle, key_value)
 	out_file = showname+"_new.srt"
 	# create a output file
 	fout = open(out_file, "w")
-	fout.write(str3)
+	fout.write(text)
 	fout.close()	
 	ensure_file_exists(out_file)
 
 def extract_clusters(filename, clusters):
+	""" Read clusters from segmentation file """
 	f = open(filename,"r")
 	for l in f:
 		 if l.startswith(";;") :
@@ -186,6 +203,7 @@ def extract_clusters(filename, clusters):
 	f.close()
 
 def mfcc_vs_gmm(showname, gmm):
+	""" Match a mfcc file and a given gmm model file """
 	commandline = 'java -Xmx2G -Xms2G -cp '+lium_jar+'  fr.lium.spkDiarization.programs.MScore --sInputMask=%s.seg   --fInputMask=%s.mfcc  --sOutputMask=%s.ident.'+gmm+'.seg --sOutputFormat=seg,UTF8  --fInputDesc="audio16kHz2sphinx,1:3:2:0:0:0,13,1:0:300:4" --tInputMask='+db_dir+'/'+gmm+' --sTop=8,ubm.gmm  --sSetLabel=add --sByCluster '+  showname 
 	start_subprocess(commandline)
 	ensure_file_exists(showname+'.ident.'+gmm+'.seg')
@@ -216,15 +234,16 @@ def wave_duration(wavfile):
 	return par[3]/par[2]
 
 def merge_waves(input_waves,wavename):
+	""" Takes a list of waves and append them all to a brend new destination wave """
 	if os.path.exists(wavename):
 		raise Exception("File gmm %s already exist!" % wavename)
 	waves = [w.replace(" ","\ ") for w in input_waves]
 	w=" ".join(waves)
 	commandline = "sox "+str(w)+" "+ str(wavename)
-	print commandline
 	start_subprocess(commandline)
 	
 def build_gmm(show,name):
+	""" Build a gmm (Gaussian Mixture Model) file from a given wave """
 	
 	diarization(show)
 
@@ -235,27 +254,14 @@ def build_gmm(show,name):
 	train_init(show)
 
 	train_map(show)
-
+	
 
 
 
 def extract_speakers(file_input):
 	cpus = cpu_count()
 	clusters = {}
-	#print '%s processors' % cpus
 	start_time = time.time()
-	#file_input = sys.argv[ 1: ] 
-	#print file_input.whitespace
-	spaces = 0
-	spaces=file_input.count(" ")
-	# if spaces > 0: #blanks in file name
-		# print file_input
-		# new_file_input = str(file_input)  
-		# new_file_input = new_file_input.replace("'",'_').replace('-','_').replace(' ','_')
-		# os.rename(file_input,new_file_input)
-		# ensure_file_exists(new_file_input)
-		# file_input=new_file_input
-		# 
 	video2trim( file_input )
 	basename, extension = os.path.splitext( file_input )
 	seg2srt(basename+'.seg')
@@ -266,27 +272,22 @@ def extract_speakers(file_input):
 	p = {}
 	files_in_db = [ f for f in os.listdir(db_dir) if f.endswith('.gmm') ]
 	for f in files_in_db:
-		#print "len(active_children()) %d" % len(active_children() )
 		if  len(active_children()) < (cpus-1) :
 			p[f] = Process(target=mfcc_vs_gmm, args=( basename, f ) )
 			p[f].start()
 		else:
 			while len(active_children()) >= cpus:
-				#print "len(active_children()) %d" % len(active_children() )
 				time.sleep(1)	
 			p[f] = Process(target=mfcc_vs_gmm, args=( basename, f ) )
 			p[f].start()
 	for proc in p:
 		if p[proc].is_alive():
-			#print 'joining proc %s' % str(proc)
 			p[proc].join()	
 	
-	#print "Processes %s" % active_children()
 	for f in files_in_db:
 		manage_ident( basename, f , clusters)
 	print ""
 	speakers = {}
-	#print clusters
 	for c in clusters:
 	    print c
 	    value = -33.0
@@ -316,7 +317,7 @@ def extract_speakers(file_input):
 
 
 def remove_blanks_callback(option, opt_str, value, parser):
-	"""Remove all white spaces in filename"""
+	"""Remove all white spaces in filename and substitute with underscores"""
 	file_input=str(parser.rargs[0])
 	new_file_input = file_input
 	new_file_input=new_file_input.replace("'",'_').replace('-','_').replace(' ','_')
@@ -343,17 +344,19 @@ def multiargs_callback(option, opt_str, value, parser):
 if __name__ == '__main__':
 	usage = "usage: %prog [options] arg"
 	parser = OptionParser(usage)
-	parser.add_option("-v", "--verbose", dest="verbose", default=False)
-	parser.add_option("-i","--input",action="callback",callback=remove_blanks_callback, metavar="FILE", help="read input video or audio file",dest="file_input")
-	parser.add_option("-g","--gmm",action="callback", callback=multiargs_callback, dest="waves_for_gmm")
-	parser.add_option("-s","--speaker",dest="speaker_for_gmm")
+	#parser.add_option("-v", "--verbose", dest="verbose", default=False)
+	parser.add_option("-i", "--input", action="callback",callback=remove_blanks_callback, metavar="FILE", help="read input video or audio file", dest="file_input")
+	parser.add_option("-g", "--gmm", action="callback", callback=multiargs_callback, dest="waves_for_gmm")
+	parser.add_option("-s", "--speaker", dest="speaker_for_gmm")
 	
 	(options, args) = parser.parse_args()
 	if len(args) == 0:
 		parser.error("incorrect number of arguments")
+	check_deps()
 	if options.file_input:
 		extract_speakers(options.file_input)
-	elif options.waves_for_gmm and options.speaker_for_gmm:
+		exit(0)
+	if options.waves_for_gmm and options.speaker_for_gmm:
 		show = None
 		waves = options.waves_for_gmm
 		speaker = options.speaker_for_gmm
@@ -366,7 +369,9 @@ if __name__ == '__main__':
 		basename, extension = os.path.splitext(w)
 		show=basename
 		build_gmm(show,speaker)
+		exit(0)
     
+	parser.print_help()
 	
 	
 	
